39270a4d98dec7e976954eef9c5e8f1143d02d44
JStroo2000 <127327594+JStroo2000@users.noreply.github.com>
2023-04-20 11:28:14 +0200

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Run handin 3" 
+
+echo "Creating plotting directory if it doesn't already exist"
+if [ ! -d "plots" ]; then
+  echo "Directory does not exist, create it!" 
+  mkdir plots
+fi
+
+echo "Download halo data"
+wget https://home.strw.leidenuniv.nl/~daalen/Handin_files/satgals_m11.txt
+wget https://home.strw.leidenuniv.nl/~daalen/Handin_files/satgals_m12.txt
+wget https://home.strw.leidenuniv.nl/~daalen/Handin_files/satgals_m13.txt
+wget https://home.strw.leidenuniv.nl/~daalen/Handin_files/satgals_m14.txt
+wget https://home.strw.leidenuniv.nl/~daalen/Handin_files/satgals_m15.txt
+
+echo "Run the script"
+python3 NUR_handin3.py > NUR_handin3.py
+
+echo "Generate the pdf"
+pdflatex main.tex
+bibtex main.aux
+pdflatex main.tex
+pdflatex main.tex
